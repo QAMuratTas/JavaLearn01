57dfe9dc5424d7d31fa650ab7a8caa84a960642a
QA Murat Tas <itmmt38@gmail.com>
2022-11-02 18:58:53 +0300

    Practice

---
+++ src/tekrarDersler/meth.java
@@ -0,0 +1,24 @@
+package tekrarDersler;
+
+public class meth {public static void multiply(int num1, int num2) {
+
+    System.out.println(num1 + num2);
+
+}
+
+
+
+    public static void multiply(int num1) {
+
+        System.out.println(num1+num1);
+
+    }
+
+
+
+    public static void multiply(double num1, double num2) {
+
+        System.out.println(num1 + num2);
+
+    }
+}
